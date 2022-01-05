2c174a8359d707653d10686795f39c0a84402e58
metehanie <metehan.bolat.ie@gmail.com>
2022-01-05 16:12:05 +0300

    Modules <-> First way -> @Binds

--- app/src/main/java/com/metehanbolat/dagger_hilt/MyInterface.kt
+++ app/src/main/java/com/metehanbolat/dagger_hilt/MyInterface.kt
@@ -1,6 +1,28 @@
 package com.metehanbolat.dagger_hilt
 
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
 interface MyInterface {
 
     fun myPrintFunction(): String
+
 }
+
+// Yol  1
+// InstallIn'de tanımlanan scope'u sınıf içerisindeki bind'da vermemiz gerekiyor.
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class MyModule {
+
+    // Burada.
+    @Singleton
+    @Binds
+    abstract fun bindingFunction(myImplementor: MyInterfaceImplementor) : MyInterface
+
+}
+
+
